73616e8de15c5f73678c2fb98f135313d8680ab9
yasmin <yaminna98@gmail.com>
2024-03-21 14:55:49 +0300

    footer changes

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,42 +1,61 @@
 <div class="footer-area">
 	<div class="container">
-		
 		<div class="row add-footer-class">
-			<div class="col-lg-6">
+			<div class="col-lg-6 col-md-6 col-sm-6">
 				<div class="footer-logo">
-					<a href="/"><img src="assets/images/home1/saman1.png" alt="logo" height="75"></a>
+					<a href="index-2.html"><img src="assets/images/home1/saman1.png" alt="logo" height="75"></a>
 				</div>
-				<p class="footer-desc">Empowering Progress
-					Illuminating Futures</p>
 				<div class="footer-contect-info">
 					<div class="footer-contact-info-title">
 						<h4>Contact</h4>
 					</div>
 					<ul>
-						<li><i class="bi bi-telephone-plus-fill"></i>+25422143367</li>
-						<li><i class="bi bi-envelope-open-fill"></i>samanenergy@gmail.com</li>
+						<li><i class="bi bi-telephone-plus-fill"></i>+25422143074</li>
+						<li><i class="bi bi-envelope-open-fill"></i>saman@gmail.com</li>
 					</ul>
 				</div>
 			</div>
-			<div class="col-lg-6 text-end">
+			<div class="col-lg-6 col-md-6 col-sm-6">
 				<div class="footer-widget-content">
 					<div class="footer-widget-title">
 						<h4>Company</h4>
 					</div>
-					<div class="footer-widget-menu ">
+					<div class="footer-widget-menu">
 						<ul>
-							<li><a href="/"><i class="fas fa-angle-right"></i>Home</a></li>
-							<li><a href="./about"><i class="fas fa-angle-right"></i>About Us</a></li>
-							<li><a href="./service"><i class="fas fa-angle-right"></i>Our Services</a></li>
-							<li><a href="./contact"><i class="fas fa-angle-right"></i>Contact</a></li>
-						
+							<li><a href="index-2.html"><i class="fas fa-angle-right"></i>Home</a></li>
+							<li><a href="about.html"><i class="fas fa-angle-right"></i>About Us</a></li>
+							<li><a href="service.html"><i class="fas fa-angle-right"></i>Our Services</a></li>
+							
 						</ul>
 					</div>
 				</div>
 			</div>			
-			
 		
+			<!-- <div class="col-lg-3 col-md-6 col-sm-6">
+				<div class="footer-widget-content">
+					<div class="footer-widget-title">
+						<h4>Newsletter</h4>
+					</div>
+					<p>Subscribe our Newsletter</p>
+					<form action="#">
+						<div class="single-newsletter-box">
+							<input type="text" name="Email" placeholder="Enter E-Mail" required>
+							<button type="submit">Subscribe Now</button>
+						</div>
+					</form>
+				</div>
+			</div> -->
 		</div>
 	</div>
-	
+	<div class="footer-bottom-area">
+		<div class="container">
+			<div class="row footer-bottom">
+				<div class="col-md-12 text-center">
+					<div class="footer-bottom-content">
+						<h4>© Copyrights 2024 Saman All rights reserved by:Saman Power</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
